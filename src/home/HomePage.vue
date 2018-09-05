5ba4752ab5a78366fac6bafc4f3fe92c3d6947a2
Mamadou <seyuf@hotmail.com>
2018-09-05 14:53:03 +0200

    configuring envs

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -12,6 +12,11 @@
 <script>
 export default {
   name: 'HomePage',
+  created() {
+    console.log(process.env.VUE_APP_TEST);
+    console.log(process.env.SOME_TEST);
+    console.log(process.env.NODE_ENV);
+  },
   props: {
     msg: String,
   },
